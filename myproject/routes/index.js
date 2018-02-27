2666747acd76740de38c21ba5c6c19b6d534c266
dalianyuan <15504055618@163.com>
2018-02-27 14:29:45 +0800

    登录静态页完成

--- myproject/routes/index.js
+++ myproject/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', {});
+});
+
+router.get('/top', function(req, res, next) {
+  res.render('top', {});
+});
+router.get('/menu', function(req, res, next) {
+  res.render('menu', {});
+});
+router.get('/main', function(req, res, next) {
+  res.render('main', {});
 });
 
 module.exports = router;
--- myproject/routes/users.js
+++ myproject/routes/users.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/top', function(req, res, next) {
+  res.render('top', {});
+});
+router.get('/menu', function(req, res, next) {
+  res.render('menu', {});
+});
+router.get('/main', function(req, res, next) {
+  res.render('main', {});
+});
+
+
 module.exports = router;
